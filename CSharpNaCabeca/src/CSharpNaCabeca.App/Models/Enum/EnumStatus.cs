a14b80495609c0581c86eef12f359fa018705df2
Rayans Carvalho Santos <rayanscs@gmail.com>
2022-01-25 23:44:56 -0300

    Crição enum exemplo

---
+++ CSharpNaCabeca/src/CSharpNaCabeca.App/Models/Enum/EnumStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace CSharpNaCabeca.App.Models.Enum
+{
+    public enum EnumStatus
+    {
+        [Description("INATIVO")]
+        Inativo = 0,
+        [Description("ATIVO")]
+        Ativo
+    }
+}
